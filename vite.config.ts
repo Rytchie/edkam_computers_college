b5ea22f18bdf586d21304bbb73b2cf4d37918cdc
Rytchie <rytchie1957@gmail.com>
2025-06-06 01:48:23 +0300

    Deployment Problems fixed

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: import.meta.env.VITE_BASE_PATH || '/edkam_computers_college',
+  base: process.env.VITE_BASE_PATH || '/edkam_computers_college'
 })
